f1a52f5a732142fa1603f4f4f5e61150ac10a061
Anlonio <antonio.machado.dev@gmail.com>
2024-11-19 00:01:36 -0300

    chore: continue project setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,43 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  compatibilityDate: "2024-11-17"
+  compatibilityDate: "2024-11-17",
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    '@vueuse/nuxt',
+    '@vite-pwa/nuxt',
+    '@nuxtjs/color-mode',
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+  ],
+  pwa: {
+    /* PWA options */
+
+  },
+  colorMode: {
+    preference: 'system', // default value of $colorMode.preference
+    fallback: 'light', // fallback value if not system preference found
+    hid: 'nuxt-color-mode-script',
+    globalName: '__NUXT_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    classPrefix: '',
+    classSuffix: '-mode',
+    storage: 'cookie', // or 'sessionStorage' or 'cookie'
+    storageKey: 'nuxt-color-mode'
+  },
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
 })
---
+++ plugins/vuetify.ts
@@ -0,0 +1,31 @@
+// import this after install `@mdi/font` package
+import '@mdi/font/css/materialdesignicons.css'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+
+import colors from 'vuetify/util/colors'
+
+export default defineNuxtPlugin((app) => {
+  const vuetify = createVuetify({
+    theme: {
+      themes: {
+        dark: {
+          dark: true,
+          colors: {
+            primary: colors.deepOrange.base,
+            secondary: colors.amber.base,
+          }
+        },
+        light: {
+          dark: false,
+          colors: {
+            primary: colors.orange.base,
+            secondary: colors.brown.base,
+          }
+        },
+      },
+    },
+  })
+  app.vueApp.use(vuetify)
+})
