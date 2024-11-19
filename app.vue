4270e54ed800d19220057f5c28428945faed11f2
Anlonio <antonio.machado.dev@gmail.com>
2024-11-19 00:03:07 -0300

    feat(pages): set initial page

--- app.vue
+++ app.vue
@@ -1,13 +1,42 @@
 <template>
-  <UContainer>
-    <UCard class="mt-10">
-      <template #header>
-        <div class="flex justify-between">
-          <h1>Welcome to Nuxt UI Starter</h1>
-          <ColorScheme><USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" /></ColorScheme>
-        </div>
-      </template>
-      <UButton icon="i-heroicons-book-open" to="https://ui.nuxt.com" target="_blank">Open Nuxt UI Documentation</UButton>
-    </UCard>
-  </UContainer>
+  <NuxtLayout>
+    <VApp>
+      <VAppBar color="primary">
+        <template v-slot:prepend>
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        </template>
+        <VAppBarTitle>{{ $route.meta.title }}</VAppBarTitle>
+        <template v-slot:append>
+          <v-btn icon="mdi-magnify"></v-btn>
+          <v-btn icon="mdi-theme-light-dark" @click="toggleTheme"></v-btn>
+          <v-btn icon="mdi-dots-vertical"></v-btn>
+        </template>
+      </VAppBar>
+      <NuxtPage />
+    </VApp>
+  </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+const mode = useColorMode()
+
+onMounted(() => {
+  if (!mode.unknown) {
+    theme.global.name.value = mode.value
+  }
+})
+
+watchEffect(() => {
+  if (!mode.unknown) {
+    theme.global.name.value = mode.value
+  }
+})
+
+function toggleTheme() {
+  mode.preference = mode.value === 'dark' ? 'light' : 'dark'
+}
+</script>
+
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <VMain>
+    <VContainer>
+      <VRow justify="center">
+        <VCol md="6" lg="4" xl="3">
+          <VCard class="mx-auto">
+            <template #title> Preciso fazer meu TCC </template>
+            <template #text>
+              <p>
+                Esse é o problema mór. Sem sua solução, nenhum outro problema
+                será solucionado nesse projeto.
+              </p>
+            </template>
+            <template #actions>
+              <VBtn color="success">Solucionar</VBtn>
+              <VTooltip text="Nem tenta, não é uma solução">
+                <template #activator="{ props }">
+                  <VBtn v-bind="props" :ripple="false" variant="plain">
+                    <VBtn color="error" disabled> Desistir </VBtn>
+                  </VBtn>
+                </template>
+              </VTooltip>
+            </template>
+          </VCard>
+        </VCol>
+      </VRow>
+    </VContainer>
+  </VMain>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  title: 'Problemas',
+  name: 'problems',
+})
+</script>
+
+<style></style>
+
